5cd2a7521db424416cc27e1ca89bb9c785967275
rafaelno-code <rafaelnc.calderon@gmail.com>
2026-04-13 22:16:24 -0500

    Added pokemon class for storage of basic pokemon attributes; added comments

--- pokeapp/app.py
+++ pokeapp/app.py
@@ -5,16 +5,18 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
+#hardcoded for now
 pokemon_name="clefairy"
 
+#Accesses pokemon GET endpoint to give holistic pokemon information
+#returns whole pokemon JSON in the form of a dictionary
 @api.route('/get_pokemon_info')
 class PokemonFinder(Resource):
     def get(self):
         pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}/")
         pokemon_json = pokemon.json()
+
         return pokemon_json
-        
-        
 
 if __name__ == '__main__':
     app.run(debug=False)
---
+++ pokeapp/pokemon.py
@@ -0,0 +1,7 @@
+class Pokemon:
+
+    #Pokemon stats to display (may be subject to change)
+    def __init__(self, name: str, weight: int, height: int):
+        self.name = name
+        self.weight = weight
+        self.height = height
